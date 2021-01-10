a1f8cbbd20eb32cda75b86a3b0ee83fed26d791d
maibaum <71893268+maibaum@users.noreply.github.com>
2021-01-10 13:41:02 +0800

    Version6
    对部分功能（包括暂存区管理、commit的生成等）进行了调整

--- version_control_git/src/HashGit/Commit.java
+++ version_control_git/src/HashGit/Commit.java
@@ -1,33 +1,18 @@
 package HashGit;
 
 public class Commit extends GitObject {
-
+    private String parentKey;
+    private String treeKey;  
+    
 	//构造函数
-    public Commit(Commit lastCommit,Tree tree) throws Exception {
-    	genValue(lastCommit,tree);
+    public Commit(String parentKey,String treeKey) throws Exception {
+    	this.parentKey=parentKey;
+    	this.treeKey=treeKey;
+    	if(parentKey!="") {
+        	this.value+="parent"+" "+this.parentKey+"\n";
+    	}
+    	this.value+="tree"+" "+this.treeKey+"\n";
     	genKey();
-    	writeFile();
-    	writeLog(); 
-    }
-    
-    //第一次commit的构造函数
-    public Commit(Tree tree)throws Exception{
-    	genValue(tree);
-    	genKey();
-    	writeFile();
-    	writeLog();
-    }
-    
-    public void genValue(Commit lastCommit,Tree tree) {
-    	this.value+="tree"+" "+tree.getKey()+"\n";
-    	this.value+="last commit"+" "+lastCommit.getKey()+"\n";
-    }
-    
-    public void genValue(Tree tree) {
-    	this.value+="tree"+" "+tree.getKey()+"\n";
-    }
-    
-    public void writeLog() {
-    	
+    	writeWithChars();
     }
 }
--- version_control_git/src/HashGit/Tree.java
+++ version_control_git/src/HashGit/Tree.java
@@ -1,27 +1,45 @@
 package HashGit;
 import java.io.*;
+import java.util.Scanner;
 
 
 public class Tree extends GitObject{
-
-    public Tree(String path) throws Exception {
-        genValue(path);
+    //根据给定的文件夹生成树
+    public Tree(String sourcePath) throws Exception {
+        this.type="tree";
+        this.sourcePath=sourcePath;
+    	genValue();
         //根据value的内容生成对应的key
         genKey();
         //写入本地文件
-        this.writeFile();
+        this.writeWithChars();
+    }
+    
+    //根据暂存区的内容生成树
+    public Tree(File index) throws Exception{
+    	this.type="tree";
+    	//根据暂存区内容生成value
+    	Scanner input=new Scanner(index);
+    	while(input.hasNextLine()) {
+    		value+=input.nextLine()+"\n";
+    	}
+    	input.close();
+    	//根据value的值生成对应的key
+    	genKey();
+    	//写入本地文件
+    	this.writeWithChars();
     }
     
     //生成value
-    public void genValue(String path)throws Exception{
-    	File file=new File(path);
+    public void genValue()throws Exception{
+    	File file=new File(this.sourcePath);
     	//根据该目录下的内容生成value
         for(File f:file.listFiles()){
             if(f.isFile()){
-                value+=  "blob " + new Blob(f).getKey() + " " + f.getName()+"\n";
+                value+=  "blob " + new Blob(sourcePath+"\\"+f.getName()).getKey() + " " + f.getName()+"\n";
             }
             else if(f.isDirectory()){
-                value+= "tree " + new Tree(f.getName()).getKey() + " " + f.getName()+"\n";
+                value+= "tree " + new Tree(sourcePath+"\\"+f.getName()).getKey() + " " + f.getName()+"\n";
             }
         }
     }
